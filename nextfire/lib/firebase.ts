7431e923761e5d59b07a6a0af3979e7ad4bc0ea5
Matthew Suddaby <msuddaby2@nait.ca>
2023-03-24 20:52:52 -0600

    nextjs

---
+++ nextfire/lib/firebase.ts
@@ -0,0 +1,45 @@
+// Import the functions you need from the SDKs you need
+
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/storage';
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDIQwq7vQwUz6J-u-AyzoRHZYt8vR11F1M",
+
+  authDomain: "nextjs-practice-98fcb.firebaseapp.com",
+
+  projectId: "nextjs-practice-98fcb",
+
+  storageBucket: "nextjs-practice-98fcb.appspot.com",
+
+  messagingSenderId: "339893477686",
+
+  appId: "1:339893477686:web:1155a344fc8d03492b5397",
+
+  measurementId: "G-KW9LSZ9Z6N"
+
+};
+
+
+// Initialize Firebase
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+// const app = initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+export const storage = firebase.storage();
---
+++ nextfire/src/pages/[username]/[slug].jsx
@@ -0,0 +1,8 @@
+export default function Post() {
+    return (
+        <main>
+            <h1>Post</h1>
+        </main>
+    )
+    
+}
